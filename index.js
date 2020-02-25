9793b3f5b908c2bb899a5c3da3c64040a8889eef
Zoe Monet Williams <mwilliamszoe@gmail.com>
2020-02-25 14:48:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-// Code your solution in this file
-function findMatching(driverList, name){
-    return driverList.filter(function (driverName){
-        return driverName.toLowerCase() === name.toLowerCase()
-    })
+// ## Write a Function To Case-insensitively Match `string`s
+
+// Write `findMatching`- This function takes an array of `drivers` and a `string` as arguments, and returns the matching list of drivers. The function should be case insensitive.
+
+function findMatching (_____, name) {
+  return list.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
 }
 
-function findMatching(driverList, name){
-    return driverList.filter(function (driverName){
-        return driverName.toLowerCase() === name.toLowerCase()
-    })
+
+// Write `fuzzyMatch` - This function takes an array of `drivers` and a `string` as arguments for querying the array, and returns all drivers whose names begin with the provided letters.
+
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
 }
 
-function fuzzyMatch(list, partialName){
-    let lengthOfName = partialName.length;
-    return list.filter(function (driverName){
-        return driverName.slice(0, lengthOfName) === partialName;
-    })
-}
+// Write `matchName` - This function takes an array of `drivers` and a `string` as arguments. In this function, each element of the `drivers` array is a JavaScript object that has a property of `name`. The function should return each element whose `name` property matches the provided `string` argument.
+
 
 function matchName (list, name) {
-    return list.filter(function (driver) {
-      return driver.name.toLowerCase() === name.toLowerCase();
-    });
-  }
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
+
